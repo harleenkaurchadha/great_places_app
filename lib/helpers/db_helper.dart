1a0d82ca62ed5361b1454a77dec76fe89c168ab3
Harleen Kaur <harleen198.chadha@gmail.com>
2020-09-25 23:52:18 +0530

    storing and fetching data with SQLite

--- lib/helpers/db_helper.dart
+++ lib/helpers/db_helper.dart
@@ -3,12 +3,24 @@ import 'package:path/path.dart' as path;
 import 'package:sqflite/sqlite_api.dart';
 
 class DBHelper{
-  static Future<void> insert(String table, Map<String, Object> data) async {
+  static Future<Database> database() async {
     final dbPath = await sql.getDatabasesPath();                                //get database path for creating new database
-    final sqlDb = await sql.openDatabase(path.join(dbPath, 'places.db'),       //to open the database if existing or create one if not there
-    onCreate: (db, version) {                                              //on create will give the access to db & the current version of new db
-     return db.execute('CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT)');
-    }, version: 1,);
-    await sqlDb.insert(table, data, conflictAlgorithm: ConflictAlgorithm.replace);
+    return sql.openDatabase(path.join(dbPath, 'places.db'),       //to open the database if existing or create one if not there
+      onCreate: (db, version) {                                              //on create will give the access to db & the current version of new db
+        return db.execute('CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT)');
+      }, version: 1,);
+  }
+  static Future<void> insert(String table, Map<String, Object> data) async {
+       final db = await DBHelper.database();
+        db.insert(
+        table,
+        data,
+        conflictAlgorithm: ConflictAlgorithm.replace
+    );
+  }
+
+  static Future<List<Map<String, dynamic>>> getData(String table) async{
+    final db = await DBHelper.database();
+    return db.query(table);
   }
 }
--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -19,10 +19,21 @@ void addPlace(String pickedTitle, File pickedImage,){
   );
   _items.add(newPlace);
   notifyListeners();
-  DBHelper.insert('places', {                                               //to add the entry in database
+  DBHelper.insert('user_places', {                                               //to add the entry in database
     'id': newPlace.id,
     'title': newPlace.title,
     'image': newPlace.image.path,
   });
+}
+
+Future<void> fetchAndSetPlaces() async{
+  final dataList = await DBHelper.getData('user_places');
+  _items = dataList.map((item) => Place(
+    id: item['id'],
+    title: item['title'],
+    image: File(item['image']),
+    location: null,
+  )).toList();
+  notifyListeners();
 }
 }
